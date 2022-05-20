3f788806cac6a6c2477837da8ed3a7b031e96330
MagnusSchH <10434660@ucn.dk>
2022-05-20 09:26:14 +0200

    product modellag

--- model/Product.java
+++ model/Product.java
@@ -1,5 +1,52 @@
 package model;
 
 public class Product {
+    private String name;
+    private int amount;
+    private int priceCost;
+    private int retailPrice;
+    private String category;
+    private String barcode;
+    private String location;
+
+    private Product(String name, int amount,int priceCost, int retailPrice, String category, String barcode, String location){
+        this.name = name;
+        this.amount = amount;
+        this.priceCost = priceCost;
+        this.retailPrice = retailPrice;
+        this.category = category;
+        this.barcode = barcode;
+        this.location = location;
+    }
+
+    //getters
+    private String getName(){
+        return name;
+    }
+
+    private int getAmount(){
+        return amount;
+    }
+
+    private int getPriceCost(){
+        return priceCost;
+    }
+
+    private int getRetailPrice(){
+        return retailPrice;
+    }
+
+    private String getCategory(){
+        return category;
+    }
+    
+    private String getBarcode(){
+        return barcode;
+    }
+    
+    private String getLocation(){
+        return location;
+    }
     
 }
+
