8986aa360532f79d508395101e6d374873f759a9
MagnusSchH <10434660@ucn.dk>
2022-05-20 10:12:49 +0200

    Update Order.java

--- model/Order.java
+++ model/Order.java
@@ -1,5 +1,15 @@
 package model;
 
 public class Order extends Sale{
+    private Costumer costomer;
+    
+    public void setCostumer(Costumer costumer){
+        this.costomer = costomer;
+    }
+    
+    public Costumer getCostumer(){
+        return costomer;
+    }
+    
     
 }
