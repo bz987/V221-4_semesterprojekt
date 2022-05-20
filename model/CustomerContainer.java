4d421597a5657d0240c23e5e3ab55f213191264a
MagnusSchH <10434660@ucn.dk>
2022-05-20 09:15:26 +0200

    Container customer og sales

--- model/CustomerContainer.java
+++ model/CustomerContainer.java
@@ -1,5 +1,35 @@
 package model;
+import java.util.HashMap;
 
 public class CustomerContainer {
+    private HashMap<String, Costumer> customers;
+    private static CustomerContainer instance;
+    
+    
+    private CustomerContainer(){
+        customers = new HashMap<>();
+    }
+    
+    public static CustomerContainer getInstance(){
+        if(instance == null){
+            instance = new CustomerContainer();
+        }
+        return instance;
+    }
+    
+    private boolean addCustomer(Costumer c, String phoneNumber){
+        if(instance != null){
+            customers.put(phoneNumber, c);
+        }else{
+            return false;
+        }
+        
+        return true;
+    }
+    
+    private Costumer getCostumer(String phoneNumber){
+        return null;
+    }
+    
     
 }
