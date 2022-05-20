61cb6150ebe46ef14a6b56914cabf6be25287a51
MagnusSchH <10434660@ucn.dk>
2022-05-20 10:02:49 +0200

    Update Warehouse.java

--- model/Warehouse.java
+++ model/Warehouse.java
@@ -1,5 +1,18 @@
 package model;
 
 public class Warehouse {
+    private String address;
     
+    
+    private Warehouse(String address){
+        this.address = address;
+    }
+    
+    private String getAddress(){
+        return address;
+    }
+    
+    private void setAddress(){
+        this.address = address;
+    }
 }
