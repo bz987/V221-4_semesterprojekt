45a03de78788b5ca0abff6bbb0ed08b6d1404600
MagnusSchH <10434660@ucn.dk>
2022-05-20 10:08:35 +0200

    Update Sale.java

--- model/Sale.java
+++ model/Sale.java
@@ -1,5 +1,30 @@
 package model;
+import java.util.ArrayList;
 
 public class Sale {
+    private ArrayList<Product> listOfProducts;
+    private int finalPrice;
+    private String date;
     
+    public Sale(int finalPrice, String date){
+        listOfProducts = new ArrayList<>();
+        this.finalPrice = finalPrice;
+        this.date = date;
+    }
+    
+    public int getFinalPrice(){
+        return finalPrice;
+    }
+    
+    public String getDate(){
+        return date;
+    }
+    
+    public void setFinalPrice(int finalPrice){
+        this.finalPrice = finalPrice;
+    }
+    
+    public void setDate(String date){
+        this.date = date;
+    }
 }
