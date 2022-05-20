c3142b1bfbb97bda75d95d0c46773bfd5deb199b
MagnusSchH <10434660@ucn.dk>
2022-05-20 10:47:11 +0200

    Update SalesAssistant.java

--- model/SalesAssistant.java
+++ model/SalesAssistant.java
@@ -1,5 +1,28 @@
 package model;
 
 public class SalesAssistant extends Person {
+    private String id;
+    private int monthlySalary;
     
+    private SalesAssistant(String name, String phoneNumber, String address, String id, int monthlySalary){
+        super(name, phoneNumber, address);
+        this.id = id;
+        this.monthlySalary = monthlySalary;
+    }
+    
+    private String getID(){
+        return id;
+    }
+    
+    private int monthlySalary(){
+        return monthlySalary;
+    }
+    
+    private void setId(){
+        this.id = id;
+    }
+    
+    private void setMonthlySalary(){
+        this.monthlySalary = monthlySalary;
+    }
 }
