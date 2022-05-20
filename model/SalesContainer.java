e3750067f0bda9e368db8c0b5599e07f733caf23
MagnusSchH <10434660@ucn.dk>
2022-05-20 09:27:16 +0200

    fejl

--- model/SalesContainer.java
+++ model/SalesContainer.java
@@ -2,28 +2,5 @@ package model;
 import java.util.HashMap;
 
 public class SalesContainer {
-    private HashMap<String, SalesAssistant> salesAssistants;
-    private static SalesAssistantContainer instance;
     
-    
-    private SalesContainer(){
-         salesAssistants= new HashMap<>();
-    }
-    
-    public static SalesAssistantContainer getInstance(){
-        if(instance == null){
-            instance = new SalesAssistantContainer();
-        }
-        return instance;
-    }
-    
-    private boolean addCustomer(SalesAssistant s, String phoneNumber){
-        if(instance != null){
-            salesAssistants.put(phoneNumber, s);
-        }else{
-            return false;
-        }
-        
-        return true;
-    }
 }
