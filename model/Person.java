881c29e1c76551736c15f900a24174a0f9572f9f
MagnusSchH <10434660@ucn.dk>
2022-05-20 10:18:45 +0200

    Update Person.java

--- model/Person.java
+++ model/Person.java
@@ -1,5 +1,25 @@
 package model;
 
 public class Person {
+    private String name;
+    private String phoneNumber;
+    private String address;
     
+    public Person(String name, String phoneNumber, String address){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    
+    public String getAddress(){
+        return address;
+    }
 }
