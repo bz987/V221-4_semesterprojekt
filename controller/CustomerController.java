3722a917c481233f44c09399a502f36b936ac546
ChrSvensson <103935675+ChrSvensson@users.noreply.github.com>
2022-05-23 10:07:42 +0200

    CustomerController
    Added methods

--- controller/CustomerController.java
+++ controller/CustomerController.java
@@ -1,5 +1,35 @@
 package controller;
+import model.*;
 
 public class CustomerController {
+    private CustomerContainer customer;
+    
+    public CustomerController(){
+        customer = customer.getInstance(); 
+    }
+    
+    public Customer findCustomer(){
+        
+    }
+    
+    public boolean createCustomer(){
+        
+    }
+    
+    public boolean updateCustomer(){
+        
+    }
+    
+    public boolean deleteCustomer(){
+        
+    }
+    
+    public boolean setCustomerDiscount(String phonenr,int discount){
+        
+    }
+    
+    public boolean setCustomerCredit(String phonenr,int amount){
+        
+    }
     
 }
