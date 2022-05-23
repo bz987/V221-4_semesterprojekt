4ede2292904ecddaf3179070e64620ff79765d6c
PentaTraxUCN <10434665@ucn.dk>
2022-05-23 12:41:43 +0200

    CustomerController updateCustomer implemented

--- controller/CustomerController.java
+++ controller/CustomerController.java
@@ -18,13 +18,39 @@ public class CustomerController {
     }
     
     public boolean updateCustomer(String phoneNumber, Customer newData){
-        boolean updated = false;
+        boolean updatedSuccessfully = false;
         Customer oldData = customer.getCustomer(phoneNumber);
-        if (phoneNumber != newData.getPhoneNumber()){
-            customer.removeCustomer(phoneNumber);
+
+        // From here it check all variables and sees if there is a new value to be added or if we can use info from the old data.
+        if (newData.getName() == null){
+            newData.setName(oldData.getName());
         }
-        
-        return updated;
+        if (newData.getPhoneNumber() == null){
+            newData.setPhoneNumber(oldData.getPhoneNumber());
+        } else { // If a new phoneNr is found, remove the customer entry in the list and set phoneNr to the new one.
+            customer.removeCustomer(phoneNumber);
+            phoneNumber = newData.getPhoneNumber();
+        }
+        if (newData.getAddress() == null){
+            newData.setAddress(oldData.getAddress());
+        }
+        if (newData.getGroup() == null){
+            newData.setGroup(oldData.getGroup());
+        }
+        if (newData.getCprNr() == null){
+            newData.setCprNr(oldData.getCprNr());
+        }
+
+         // since this is only changeable by the manager, this will not change here.
+        newData.setDiscount(oldData.getDiscount());
+        newData.setCredit(oldData.getCredit());
+
+        customer.addCustomer(newData, phoneNumber);
+        if (customer.getCustomer(phoneNumber).equals(newData)){ // quick check to see if the phoneNr now has the newData in the container
+            updatedSuccessfully = true;
+        }
+
+        return updatedSuccessfully;
     }
     
     public boolean deleteCustomer(String phoneNumber){
