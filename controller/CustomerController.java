c49ad583a584aae8d0f42ab516005233cb9233d1
ChrSvensson <103935675+ChrSvensson@users.noreply.github.com>
2022-05-23 10:45:08 +0200

    CustomerController
    findCustomer(), createCustomer(), deleteCustomer() burde være færdige.

--- controller/CustomerController.java
+++ controller/CustomerController.java
@@ -8,28 +8,31 @@ public class CustomerController {
         customer = customer.getInstance(); 
     }
     
-    public Customer findCustomer(){
-        
+    public Customer findCustomer(String phoneNumber){
+        Customer custom = customer.getCustomer(phoneNumber);
+        return custom;
     }
     
-    public boolean createCustomer(){
-        
+    public boolean createCustomer(Customer c, String phoneNumber){
+        boolean isAdded = customer.addCustomer(c, phoneNumber);
+        return isAdded;
     }
     
     public boolean updateCustomer(){
-        
+        return false;
     }
     
-    public boolean deleteCustomer(){
-        
+    public boolean deleteCustomer(String cprNr){
+        boolean isRemoved = customer.removeCustomer(cprNr);
+        return isRemoved;
     }
     
     public boolean setCustomerDiscount(String phonenr,int discount){
-        
+        return false;
     }
     
     public boolean setCustomerCredit(String phonenr,int amount){
-        
+        return false;
     }
     
 }
