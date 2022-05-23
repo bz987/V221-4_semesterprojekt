3f0cd97339276e8a662b9e96a68654acb30efa62
Erik <10407515@ucn.dk>
2022-05-23 10:42:39 +0200

    stubs salescontroller

--- controller/SalesController.java
+++ controller/SalesController.java
@@ -1,5 +1,35 @@
 package controller;
+import model.*;
 
 public class SalesController {
- 
+
+    private SalesContainer salesContainer;
+    
+    public SalesController(){
+        salesContainer = salesContainer.getInstance();
+    }
+    
+    public boolean createSale(){
+        return true;
+    }
+    
+    public boolean updateSale(){
+        return true;
+    }
+    
+    public boolean deleteSale(){
+        return true;
+    }
+    
+    public boolean createorder(){
+        return true;
+    }
+    
+        public boolean updateOrder(){
+        return true;
+    }
+    
+    public boolean deleteOrder(){
+        return true;
+    }
 }
