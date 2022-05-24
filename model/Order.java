8ff70edb6a7f53a83fe5221fd2afd4e927924d86
Erik <10407515@ucn.dk>
2022-05-24 09:52:52 +0200

    order update salesNumber

--- model/Order.java
+++ model/Order.java
@@ -3,8 +3,8 @@ package model;
 public class Order extends Sale{
     private Customer customer;
     
-    public Order(int finalPrice, String date){
-        super(finalPrice,date);
+    public Order(int finalPrice, String date, String salesNumber){
+        super(finalPrice,date, salesNumber);
     }
     
     public void setCustomer(Customer customer){
