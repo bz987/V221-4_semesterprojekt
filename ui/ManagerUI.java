f18064b17abe1a971c92c06e340c23d46c398d0f
MagnusSchH <10434660@ucn.dk>
2022-05-24 11:01:53 +0200

    Start på ui

--- ui/ManagerUI.java
+++ ui/ManagerUI.java
@@ -1,5 +1,57 @@
 package ui;
+import controller.*;
+import java.util.Scanner;
 
 public class ManagerUI {
+    private SalesController salesController;
+    private ProductController productController;
 
+    /**
+     * Constructor for objects of class LoanMenu
+     */
+    public ManagerUI() {
+        // initialise instance variables
+        productController = new ProductController();
+        salesController = new SalesController();
+    }
+
+    public void start() {
+        ManagerMenu();
+    }
+
+    private void ManagerMenu() {
+        boolean running = true;
+        while (running) {
+            int choice = writeLoanMenu();
+            switch (choice) {
+                case 1:
+                  System.out.println(" Denne er ikke implementeret endnu!");
+                  break;
+                case 0:
+                  running = false;
+                  break;
+                default:
+                  System.out.println("En uforklarlig fejl er sket med choice = " + choice);
+                  break;
+            }
+        }
+    }
+    
+    private int writeManagerMenu() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("****** Udlånsmenu ******");
+        System.out.println(" (1) Opret lån");
+        System.out.println(" (0) Tilbage");
+        System.out.print("\n Vælg:");
+        int choice = getIntegerFromUser(keyboard);
+        return choice;
+    }
+    
+    private int getIntegerFromUser(Scanner keyboard) {
+        while (!keyboard.hasNextInt()) {
+            System.out.println("Input skal være et tal - prøv igen");
+            keyboard.nextLine();
+        }
+        return keyboard.nextInt();
+    }
 }
--- ui/SalesUI.java
+++ ui/SalesUI.java
@@ -1,5 +1,65 @@
 package ui;
+import controller.*;
+import java.util.Scanner;
 
 public class SalesUI {
-    
+    private SalesController salesController;
+    private ProductController productController;
+
+    /**
+     * Constructor for objects of class LoanMenu
+     */
+    public SalesUI() {
+        // initialise instance variables
+        productController = new ProductController();
+        salesController = new SalesController();
+    }
+
+    public void start() {
+        productMenu();
+    }
+
+    private void productMenu() {
+        boolean running = true;
+        while (running) {
+            int choice = writeLoanMenu();
+            switch (choice) {
+                case 1:
+                    System.out.println(" Denne er ikke implementeret endnu!");
+                    break;
+                case 2:
+                    System.out.println();
+                    break;
+                case 3:
+                    System.out.println();
+                    break;
+                case 0:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("En uforklarlig fejl er sket med choice = " + choice);
+                    break;
+            }
+        }
+    }
+
+    private int writeLoanMenu() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("****** Salgssmenu ******");
+        System.out.println(" (1) Opret salg");
+        System.out.println(" (2) Opret order");
+        System.out.println(" (3) Find product");
+        System.out.println(" (0) Tilbage");
+        System.out.print("\n Vælg:");
+        int choice = getIntegerFromUser(keyboard);
+        return choice;
+    }
+
+    private int getIntegerFromUser(Scanner keyboard) {
+        while (!keyboard.hasNextInt()) {
+            System.out.println("Input skal være et tal - prøv igen");
+            keyboard.nextLine();
+        }
+        return keyboard.nextInt();
+    }
 }
