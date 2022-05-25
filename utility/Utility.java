ecee14c0fc333b05574cd1bfb3c414a1825005d2
PentaTraxUCN <10434665@ucn.dk>
2022-05-25 11:13:59 +0200

    Utility 0.1

--- utility/Utility.java
+++ utility/Utility.java
@@ -1,7 +1,11 @@
 package utility;
 
 public class Utility {
+
+    // takes a value and limits it between a min and max value
     public static int clamp(int value, int min, int max){
         return Math.max(min, Math.min(max, value));
     }
+
+
 }
