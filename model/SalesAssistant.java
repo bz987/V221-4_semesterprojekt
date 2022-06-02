72c6db2b7b576128ec8b7f1f43baf2296cf5af72
bz987 <102587713+bz987@users.noreply.github.com>
2022-06-02 11:11:08 +0200

    Yes

--- model/SalesAssistant.java
+++ model/SalesAssistant.java
@@ -3,11 +3,13 @@ package model;
 public class SalesAssistant extends Person {
     private String id;
     private int monthlySalary;
+    private int amountOfSales;
     
     public SalesAssistant(String name, String phoneNumber, String address, String id, int monthlySalary){
         super(name, phoneNumber, address);
         this.id = id;
         this.monthlySalary = monthlySalary;
+        this.amountOfSales = 0;
     }
     
     public String getID(){
@@ -24,5 +26,8 @@ public class SalesAssistant extends Person {
     
     public void setMonthlySalary(int monthlySalary){
         this.monthlySalary = monthlySalary;
+    }
+    public void addSale(){
+        this.amountOfSales+=1;
     }
 }
